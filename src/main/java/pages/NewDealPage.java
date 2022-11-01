1bad4f6926f4d7479230db8fe59a632d1a6c520f
Prasad Sutar <sutarprasad8547@gmail.com>
2022-11-01 19:12:24 +0530

    this is my first commit

--- src/main/java/pages/NewDealPage.java
+++ src/main/java/pages/NewDealPage.java
@@ -1,5 +1,24 @@
 package pages;
 
-public class NewDealPage {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
+import base.TestBase;
+
+public class NewDealPage extends TestBase{
+	
+	//elelemnt initialization
+	public NewDealPage() {
+		PageFactory.initElements(driver, this);
+	}
+	
+	
+	
+//Element repo
+	@FindBy (xpath = "")
+	WebElement element;
+	
+	//Action
+	
 }
